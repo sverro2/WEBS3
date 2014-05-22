a4545a3ee2fcf9a920180cb1c5ba65efa77c9336
Yannik Hegge <yannikhegge@gmail.com>
2014-05-23 01:33:04 +0200

    simple html5 form validation added

--- app/views/layout/elements/login.blade.php
+++ app/views/layout/elements/login.blade.php
@@ -9,10 +9,10 @@
       <div class="modal-body">
           {{ Form::open(array('url' => 'account/login', 'class'=>'form col-md-12 center-block')) }}
             <div class="form-group">
-              {{ Form::text('username', null, array('placeholder'=>'Gebruikersnaam', 'class'=>'form-control input-lg')) }}
+              {{ Form::text('username', null, array('placeholder'=>'Gebruikersnaam', 'class'=>'form-control input-lg', 'required'=>'')) }}
             </div>
             <div class="form-group">
-              {{ Form::password('password', array('placeholder' => 'Wachtwoord', 'class'=>'form-control input-lg')) }}
+              {{ Form::password('password', array('placeholder' => 'Wachtwoord', 'class'=>'form-control input-lg', 'required'=>'')) }}
             </div>
             <div class="form-group">
               {{ Form::submit('Aanmelden', array('class'=>'btn btn-primary btn-lg btn-block')) }}
