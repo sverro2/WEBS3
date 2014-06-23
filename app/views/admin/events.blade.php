a9aab83e29569d0482e2b907ba8ca57dfecf6201
Sven Brettschneider <brettschneider.sve@gmail.com>
2014-06-23 12:35:26 +0200

    verwijderen toevoegen in admin voor evenementen en cleanup

--- app/views/admin/events.blade.php
+++ app/views/admin/events.blade.php
@@ -11,4 +11,27 @@
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.removeEvent').click(function(event){
+			var orgUrl = $(this).data('org-url');
+			var clicked = $(this);
+			event.preventDefault();
+			if(confirm("Weet je zeker dat je dit evenement wil verwijderen?")){
+				var eventId = $(this).data('eventid');
+				
+				$.post(orgUrl, {event_id: eventId}).done(function(status){
+					if(status == 'done'){
+						clicked.closest('tr').hide();
+					}else{
+						alert('Het evenement is niet verwijdert wegens een onbekende fout :-/');
+					}
+				});
+			}
+
+		});
+	});
+
+</script>
 @stop
